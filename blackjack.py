4d5bbdc82f617d67d5136a5cf0fcc05b3d9f2525
anuthomson <anumol.thomas94@ed.cna.nl.ca>
2025-11-30 00:26:09 -0330

    Added starting files (money.txt, db.py) and created suits/ranks/values lists. Also wrote read_money() and write_money().

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,18 @@
+from db import read_money,write_money
+
+suits=["Hearts","Diamonds","Clubs","Spades"]
+ranks= ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
+values = [2,3,4,5,6,7,8,9,10,11]
+
+
 
 
 def main():
-    # display the welcome message
-    print("Welcome to Blackjack!")
+    print(" BlackJACK!")
+    print("Blackjack payout is 3:2")
+    money=read_money()
+
+
 
 
 
---
+++ db.py
@@ -0,0 +1,14 @@
+#db.py
+#set file name into a variable
+filename="money.txt"
+def read_money(filename):
+    # read  data from file
+    with open(filename, 'r') as file:
+        money = file.readlines()
+    return money
+
+def write_money(money, filename):
+   # write data to file
+    with open(filename, 'w') as file:
+        for line in money:
+            file.write(line)
